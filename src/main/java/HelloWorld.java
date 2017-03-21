b670d695b649504d62f3f971716ae306c83be44c
erumbold <erumbold@ithaca.edu>
2017-03-21 19:33:40 -0400

    created project

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+import static spark.Spark.*;
+
+/**
+ * Created by erikarumbold on 3/21/17.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World");
+    }
+}
