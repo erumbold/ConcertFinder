23c3762c45068147d9e7baa6cfa90c33912b5b32
kizito <kumunak1@ithaca.edu>
2017-03-29 05:58:51 -0400

    my test function

---
+++ src/main/java/insertuser.java
@@ -0,0 +1,9 @@
+/**
+ * Created by kumun_000 on 3/29/2017.
+ */
+public class insertuser {
+    public String insertuser(String Name,String username, String password, int age, String email){
+        String x= "";
+        return x;
+    }
+}
---
+++ test/main/java/MyFirstTest.java
@@ -0,0 +1,21 @@
+/**
+ * Created by kumun_000 on 3/27/2017.
+ */
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class MyFirstTest {
+    @Test
+    public void insertuserTest() {
+        insertuser iu= new insertuser();
+        String result= iu.insertuser("Kizito Umunakwe", "kizisaurus", "kizito23",22,"umunakwec@yahoo.com");
+        assertEquals("Kizito Umunakwe", result);
+
+        result=iu.insertuser("Alex Mercer", "Merc","Alex345",20,"AlecMerc@yahoo.com" );
+        assertEquals("Alex Mercer", result);
+    }
+
+
+}
