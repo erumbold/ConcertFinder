e618b066fd1e468b158241041647df8c25e044f5
erumbold <erumbold@ithaca.edu>
2017-04-03 02:01:59 -0400

    finished Database API, docstrings on functions

--- src/main/java/Controller/EventManager.java
+++ src/main/java/Controller/EventManager.java
@@ -1,16 +1,31 @@
 package Controller;
 
-import Controller.Database;
-
 /**
  * Created by erikarumbold on 3/27/17.
  */
 public class EventManager {
 
-    public static void main(String[] args) {
+    public EventManager(){
         Database db = new Database();
-        String result = db.insertEvent("Firefly Music Festival", 6, 18, 2017, 12, 0, "description",
-                "The Woodlands", "123 Place", "Dover", "DE", "19901", -.5, .5);
-        System.out.println(result);
+    }
+
+    public void insertEvent(){
+
+    }
+
+    public void modifyEvent(){
+
+    }
+
+    public void selectEvent(){
+
+    }
+
+    public void deleteEvent(){
+
+    }
+
+    public static void main(String[] args) {
+        EventManager em = new EventManager();
     }
 }
