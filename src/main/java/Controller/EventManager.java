515b468e3d90dff51dd6411ec36882c7a55443eb
Seth <sosegha01@gmail.com>
2017-04-03 10:11:14 -0400

    Search Integration Stuff

--- src/main/java/Controller/EventManager.java
+++ src/main/java/Controller/EventManager.java
@@ -1,6 +1,7 @@
 package Controller;
 
 import Controller.Database;
+import Model.Search;
 
 /**
  * Created by erikarumbold on 3/27/17.
@@ -12,5 +13,8 @@ public class EventManager {
         String result = db.insertEvent("Firefly Music Festival", 6, 18, 2017, 12, 0, "description",
                 "The Woodlands", "123 Place", "Dover", "DE", "19901", -.5, .5);
         System.out.println(result);
+
+        Search mySearch = new Search();
+        mySearch.findEvent();
     }
 }
--- src/main/java/Model/Search.java
+++ src/main/java/Model/Search.java
@@ -1,41 +1,62 @@
 package Model; /**
  * Created by Seth on 3/22/2017.
  */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import Controller.Database;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
-import javafx.stage.Stage;
 
-public class Search extends Application {
+public class Search{
+    public Database db = new Database();
+    public String parameter;
+    public String [] events;
 
-    public static void main(String[] args) {
-        Application.launch(args);
+    public Search()
+    {
+        String result = db.insertEvent("Firefly Music Festival", 6, 18, 2017, 12, 0, "description",
+                "The Woodlands", "123 Place", "Dover", "DE", "19901", -.5, .5);
+        String result2 = db.insertEvent("Dram at Ithaca College", 4, 25, 2017, 11, 0, "description",
+            "A and E Center", "953 Danby", "Ithaca", "NY", "27501", -.5, .5);
     }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        primaryStage.setTitle("Concert Finder");
+    public boolean findEvent()
+    {
+        System.out.println("Enter the name of an event you're looking for:");
+        System.out.println();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String[]> result;
+        try {
+            parameter = br.readLine();
+            result = db.selectEventByTitle(parameter);
+            events = result.get(0);
 
-        TextField textField = new TextField();
-        textField.setPrefWidth(360);
+            if(result.size() > 0)
+            {
+                System.out.print("Found:  ");
+                for(int i = 0; i < events.length; i++)
+                {
+                    if(i < (events.length - 1))
+                    {
+                        System.out.print(events[i] + ", ");
+                    }else
+                    {
+                        System.out.print(events[i]);
+                    }
+                }
+                System.out.println();
+                return true;
+            }else
+            {
+                System.out.println("Event does not exist.");
+                return false;
+            }
 
+        } catch (IOException e) {
+            System.out.println(e);
+            return false;
+        }
 
-
-        Button button = new Button("Enter Band Name");
-
-        button.setOnAction(action -> {
-            String aConcert = textField.getText();
-            System.out.println(aConcert +" will be peforming tonight at 8:00pm.");
-            System.out.println("Location: 355, College View Avenue.");
-        });
-
-        HBox hbox = new HBox(textField, button);
-
-        Scene scene = new Scene(hbox, 500, 100);
-        primaryStage.setScene(scene);
-        primaryStage.show();
     }
 }
